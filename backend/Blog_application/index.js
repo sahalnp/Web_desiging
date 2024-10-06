9c7b15443d59d08a152e3455194c7180b3c45551
Muahmmed Sahal <200~npmuhammedsahal@gmail.com>
2024-10-06 09:33:19 +0530

    added command

--- backend/Blog_application/index.js
+++ backend/Blog_application/index.js
@@ -1,22 +1,27 @@
+// Initialize Express app
 import express from "express";
 import bodyParser from "body-parser";
 
+// Initialize Express app
 const app=express();
 const port=3000;
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
+// Data storage
 let posts=[];
 
 app.get("/",(req,res)=>{
     res.render("home.ejs",{posts})
 })
 
+//creating homne page
 app.get("/create",(req,res)=>{
     res.render("create.ejs");
 })
+//Submitting the blog tile and content
 app.post("/submit",(req,res)=>{
     const post={
         title:req.body.title,
@@ -26,6 +31,7 @@ app.post("/submit",(req,res)=>{
     res.redirect('/')
     console.log(posts)
 })
+//showing the blog
 app.get('/post/:id',(req,res)=>{
     const postId=req.params.Id;
     const post  = posts[postid];
@@ -38,6 +44,7 @@ app.get('/post/:id',(req,res)=>{
         res.send("page not found");
     }
 })
+// showing editing page
 app.get("/post/:id/edit",(req,res)=>{
     const postid=req.params.id;
     const post = posts[postid];
@@ -51,7 +58,8 @@ app.get("/post/:id/edit",(req,res)=>{
         res.send("page not found");
     }
 })
-app.post("/post/:id/edit",(req,res)=>{ //editing post 
+//Editing the blog title and content
+app.post("/post/:id/edit",(req,res)=>{ 
     const postid=req.params.id;
 
     posts[postid].title=req.body.title;
@@ -59,6 +67,8 @@ app.post("/post/:id/edit",(req,res)=>{ //editing post
 
     res.redirect(`/post/${postId}`);
 })
+
+//Delelting the blog
 app.post("/post/:id/delete",(req,res)=>{
     const postId=req.params.id;
     posts.splice(postId, 1);
